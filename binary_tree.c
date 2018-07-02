14e323d104fb878223c499c4a929aac220ada91f
azhry <arliansyah_azhary@yahoo.com>
2018-07-02 14:30:09 +0700

    Added comments

--- binary_tree.c
+++ binary_tree.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Definisikan tipe data untuk merepresentasikan node
 typedef struct Node {
     int data;
     struct Node* left;
     struct Node* right;
 } Node;
 
+// Fungsi untuk membuat node baru
 Node* newNode(int newData)
 {
-    Node* node = malloc(sizeof(Node));
-    node->data = newData;
-    node->left = NULL;
-    node->right = NULL;
-    return node;
+    Node* node = malloc(sizeof(Node)); // alokasi memori utk node baru
+    node->data = newData; // beri data ke node baru
+    node->left = NULL; // null-kan terlebih dahulu
+    node->right = NULL; // null-kan terlebih dahulu
+    return node; // balikkan node yg telah dibuat
 }
 
-Node* root = NULL;
+Node* root = NULL; // inisialiasi node paling atas (root/akar) dengan null
 
 void insertNewNode(int newData)
 {
-    if (root == NULL)
+    if (root == NULL) // apakah tree kosong ?
     {
-        root = newNode(newData);
+        root = newNode(newData); // jadikan node baru tersebut sebagai akar
         printf("Root inserted: %d\n", root->data);
         return;
     }
@@ -30,32 +32,33 @@ void insertNewNode(int newData)
     {
         printf("Traverse %d..\n", newData);
 
-        Node* currentNode = root;
-        while (currentNode != NULL)
+        Node* currentNode = root; // inisialisasi variabel pembantu untuk melintasi tree dengan start-nya di akar
+
+        while (currentNode != NULL) // traverse selama currentNode tidak null atau tidak kosong
         {
-            if (newData < currentNode->data)
+            if (newData < currentNode->data) // apakah data yang akan dimasukkan lebih kecil dari data yang ada di currentNode
             {
-                if (currentNode->left == NULL)
+                if (currentNode->left == NULL) // cek apakah left child dari currentNode kosong atau null
                 {
-                    currentNode->left = newNode(newData);
-                    printf("Left inserted %d\n", currentNode->data);
+                    currentNode->left = newNode(newData); // buat node baru dan jadikan sebagai left child dari currentNode
+                    printf("Left inserted %d\n", currentNode->left->data);
                     return;
                 }
 
                 printf("Go left..\n");
-                currentNode = currentNode->left;
+                currentNode = currentNode->left; // currentNode pergi ke left child
             }
             else
             {
-                if (currentNode->right == NULL)
+                if (currentNode->right == NULL) // cek apakah right child dari currentNode kosong atau null
                 {
-                    currentNode->right = newNode(newData);
-                    printf("Right inserted %d\n", currentNode->data);
+                    currentNode->right = newNode(newData); // buat node baru dan jadikan sebagai right child dari currentNode
+                    printf("Right inserted %d\n", currentNode->right->data);
                     return;
                 }
 
                 printf("Go right..\n");
-                currentNode = currentNode->right;
+                currentNode = currentNode->right; // currentNode pergi ke right child
             }
         }
     }
@@ -65,33 +68,36 @@ void insertNewNode(int newData)
 
 void preOrder(Node* node)
 {
-    if (node == NULL)
+    if (node == NULL) // base case: preOrder stop
         return;
 
     printf("%d ", node->data);
-    preOrder(node->left);
-    preOrder(node->right);
+    preOrder(node->left); // traverse ke left child
+    preOrder(node->right); // traverse ke right child
 }
 
 void inOrder(Node* node)
 {
-    if (node == NULL)
+    if (node == NULL) // base case: inOrder stop
         return;
 
-    inOrder(node->left);
+    inOrder(node->left); // traverse ke left child
     printf("%d ", node->data);
-    inOrder(node->right);
+    inOrder(node->right); // traverse ke right child
 }
+
+// base case itu adalah kondisi dimana fungsi rekursif harus berhenti
 
 void postOrder(Node* node)
 {
-    if (node == NULL)
+    if (node == NULL) // base case: postOrder stop
         return;
 
-    postOrder(node->left);
-    postOrder(node->right);
+    postOrder(node->left); // traverse ke left child
+    postOrder(node->right); // traverse ke right child
     printf("%d ", node->data);
 }
+
 
 int main()
 {
@@ -103,19 +109,19 @@ int main()
         insertNewNode(dataArr[i]);
     }
 
-    Node* currentNode = root;
+    Node* currentNode = root; // awali currentNode dari root
     printf("Pre order: ");
-    preOrder(currentNode);
+    preOrder(currentNode); // tampilkan secara preOrder
     printf("\n");
 
-    currentNode = root;
+    currentNode = root; // awali currentNode dari root
     printf("In order: ");
-    inOrder(currentNode);
+    inOrder(currentNode); // tampilkan secara inOrder
     printf("\n");
 
-    currentNode = root;
+    currentNode = root; // awali currentNode dari root
     printf("Post order: ");
-    postOrder(currentNode);
+    postOrder(currentNode); // tampilkan secara postOrder
     printf("\n");
 
     return 0;
